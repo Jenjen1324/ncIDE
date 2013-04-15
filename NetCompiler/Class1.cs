d12f1bae68767e8ddeeb03acecf644a43c4473ea
Northcode <northcode.no@gmail.com>
2013-04-15 13:26:38 +0200

    initial publish

---
+++ NetCompiler/Class1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.CSharp;
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCompiler
+{
+    public class CsCompiler
+    {
+        CSharpCodeProvider csc = new CSharpCodeProvider(new Dictionary<string, string>() { { "CompilerVersion", "v3.5" } });
+
+        public string[] Referances { get; set; }
+        public string Name { get;set; }
+        public string[] Files { get; set; }
+
+        public CompilerErrorCollection Errors { get; set; }
+
+        public void Compile()
+        {
+            var pars = new CompilerParameters(Referances, Name, true);
+            pars.GenerateExecutable = true;
+            CompilerResults result = csc.CompileAssemblyFromFile(pars, Files);
+            Errors = result.Errors;
+        }
+
+    }
+}
---
+++ Projects/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projects
+{
+    public class Project
+    {
+        virtual public void Save(string file);
+
+        virtual public static Project Load(string file);
+    }
+}
